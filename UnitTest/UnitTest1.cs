9cd56d5d0a1b885a57aead83dd796a474b82f8fd
Ivan Lidovskoi <90608498+DeLarge666@users.noreply.github.com>
2022-07-03 15:00:55 +0300

    Add files via upload

---
+++ UnitTest/UnitTest1.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using CoreLib;
+
+namespace UnitTestStudNumber
+{
+    [TestClass]
+    public class StudNumberCheckerTests
+    {
+        [TestMethod]
+        public void CheckFioSymbols()
+        {
+            //Arrange
+            string fio = "Иванов Иван Иванович";
+            bool expected = true;
+            //Act
+            bool actual = 
+            //Assert
+        }
+    }
+}

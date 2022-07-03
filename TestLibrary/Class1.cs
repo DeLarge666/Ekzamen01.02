f22ae798a7b517bc57c17f5083856c895b7a92fa
Ivan Lidovskoi <90608498+DeLarge666@users.noreply.github.com>
2022-07-03 14:59:51 +0300

    Add files via upload

---
+++ TestLibrary/Class1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoreLib
+{
+    public static class Students
+    {
+        public static string GetStudNumber(string fio, DateTime year, int group)
+        {
+            var fullname = fio.Split(' ');
+            return $"{year.ToString("yyyy")}.{group.ToString("1234567890")}.{fullname[0]} {fullname[1]} {fullname[2]}";
+        }
+
+        public static bool ValidateStudNumber(string fio)
+        {
+            if (!fio.Any(Char.IsLower))
+                return false;
+           
+            return true;
+        }
+    }
+}
